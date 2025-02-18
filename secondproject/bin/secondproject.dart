00d8524b4f61fb98d7b9e836d4b13762e63ed7c3
Muhammad Abdullah <39946504+abdullahwale@users.noreply.github.com>
2025-02-18 09:32:55 +0500

    add interest calculator

--- secondproject/bin/secondproject.dart
+++ secondproject/bin/secondproject.dart
@@ -1,5 +1,24 @@
-import 'package:secondproject/secondproject.dart' as secondproject;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${secondproject.calculate()}!');
+// Function to calculate simple interest
+double calculateSimpleInterest(double principal, double rate, double time) {
+  return (principal * rate * time) / 100;
+}
+
+void main() {
+  // Taking user input
+  stdout.write("Enter Principal Amount: ");
+  double principal = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter Rate of Interest (%): ");
+  double rate = double.parse(stdin.readLineSync()!);
+
+  stdout.write("Enter Time (in years): ");
+  double time = double.parse(stdin.readLineSync()!);
+
+  // Calculate interest
+  double interest = calculateSimpleInterest(principal, rate, time);
+
+  // Display result
+  print("\nSimple Interest: \$${interest.toStringAsFixed(2)}");
 }
